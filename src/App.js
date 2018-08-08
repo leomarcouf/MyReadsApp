acd50c16030aaea1416d21c802b1d661897106eb
DemanPierre <leomarcouf@hotmail.com>
2018-08-08 18:53:23 +0200

    ajout du composant searchPage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Main from './Main'
+import SearchPage from './SearchPage'
 
 import * as BooksAPI from './BooksAPI'
 import './App.css'
@@ -30,6 +31,10 @@ class BooksApp extends Component {
     this.setState({ showSearchPage: true })
   }
 
+  closeSearch = () => {
+    this.setState({ showSearchPage: false })
+  }
+
   /**
    * The following Method to update the shelf is a simplified variation of
    * the method from Maeva's study jam: https://youtu.be/i6L2jLHV9j8 
@@ -52,25 +57,9 @@ class BooksApp extends Component {
       console.log(this.state.books),
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchPage
+            closeSearch={this.closeSearch.bind(this)}
+          />
         ) : (
           <Main
             books={this.state.books}
